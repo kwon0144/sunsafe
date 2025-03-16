11c4c2854cde7abf568351b46bf435e9ca7a054d
kwon0144 <kinsey.w5505@gmail.com>
2025-03-16 19:14:32 +1100

    uxui: adjust padding for responsiveness

--- frontend/src/pages/UVIndexTracker/index.tsx
+++ frontend/src/pages/UVIndexTracker/index.tsx
@@ -7,7 +7,7 @@ import UVLevelGuide from "./UVLevelGuide";
 const UVIndexTracker = () => {
 
   return (
-    <div className="min-h-screen max-w-7xl mx-auto py-30">
+    <div className="min-h-screen max-w-7xl mx-auto py-30 px-10">
       {/* Title */}
       <Title title="UV Index Tracker" description="Track the UV index for your location" />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mb-10">
