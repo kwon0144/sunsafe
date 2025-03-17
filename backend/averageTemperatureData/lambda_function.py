1bbfb9a1ed15e1d82361e7e880dd6408a8335548
kwon0144 <kinsey.w5505@gmail.com>
2025-03-17 11:26:03 +1100

    backend: lambda function to fetch temperature data

---
+++ backend/averageTemperatureData/lambda_function.py
@@ -0,0 +1,46 @@
+import os
+import mysql.connector
+import json
+
+def lambda_handler(event, context):    
+    try:
+        # Establish database connection
+        conn = mysql.connector.connect(
+            host = os.environ["DB_HOST"], 
+            user = os.environ["DB_USER"],       
+            password = os.environ["DB_PASSWORD"],
+            database = os.environ["DB_NAME"]
+        )
+        
+        cursor = conn.cursor()
+        query = """SELECT month, ROUND(AVG(temperature)) FROM temperature GROUP BY month;"""
+        
+        cursor.execute(query)
+        result = cursor.fetchall()
+        
+        # Extract years and counts into separate lists
+        response = {
+            'year': [row[0] for row in result],
+            'count': [row[1] for row in result]
+        }
+
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+        
+        return {
+            'statusCode': 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+            },
+            'body': json.dumps(response)
+        }
+    
+    except mysql.connector.Error as err:
+        return {
+            'statusCode': 500,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+            },
+            'body': json.dumps({'error': str(e)})
+        }
