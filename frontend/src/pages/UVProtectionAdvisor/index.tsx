d84d57db53ac02192844e913a1d07c048632da5a
kwon0144 <kinsey.w5505@gmail.com>
2025-03-20 16:36:30 +1100

    ui: update style for uv protection advisor iframe

--- frontend/src/pages/UVProtectionAdvisor/index.tsx
+++ frontend/src/pages/UVProtectionAdvisor/index.tsx
@@ -2,22 +2,23 @@ import Title from "../../components/Title";
 
 const UVProtectionAdvisor = () => {
   return (
-    <div className="min-h-screen max-w-7xl mx-auto py-30 px-10">
+    <>
+    <div className="max-w-7xl mx-auto pt-30 px-10">
       {/* Title */}
       <Title title="UV Protection Advisor" description="Get personalized recommendations for your skin type and lifestyle." />
-      {/* Main Content */}
-      <div className="w-full h-[1200px] md:h-[1100px] lg:h-[800px] overflow-hidden">
-        <iframe
-          src="https://richardxiong-demo-app.hf.space"
-          className="w-full h-full"
-          frameBorder="0"
-          scrolling="no"
-          style={{ overflow: 'hidden' }}
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-      </div>
     </div>
+    <div className="max-w-7xl mx-auto h-[1100px] md:h-[750px] overflow-hidden">
+      <iframe
+        src="https://richardxiong-demo-app.hf.space"
+        className="w-full h-full"
+        frameBorder="0"
+        scrolling="no"
+        style={{ overflow: 'hidden' }}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+  </div>
+  </>
   );
 };
 
